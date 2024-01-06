350012bcf5523988c63e96633325d6d33af7ff18
Francis Rubio <francis.rubio@dice205.com>
2024-01-07 04:01:32 +0800

    Added links page

---
+++ src/data/links.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'RelativeTimeFormat',
+    url: 'https://www.youtube.com/watch?v=NC9SlnIdH-U',
+    cover: {
+      folder: 'relative-time-format',
+      filename: 'instagram-thumbnail.png',
+      sizes: [300, 600, 900, 1080],
+      formats: ['png', 'webp', 'avif'],
+      width: 1080,
+      height: 1080
+    }
+  }
+]
