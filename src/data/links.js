192e4ad6d953897694792065640fc56e8269e40f
Francis Rubio <francis.rubio@dice205.com>
2024-06-02 15:25:15 +0800

    Add link

--- src/data/links.js
+++ src/data/links.js
@@ -2,6 +2,7 @@ module.exports = [
   {
     title: 'RelativeTimeFormat',
     url: 'https://www.youtube.com/watch?v=NC9SlnIdH-U',
+    date: new Date(2023, 0, 22),
     cover: {
       folder: 'relative-time-format',
       filename: 'instagram-thumbnail.png',
@@ -14,6 +15,7 @@ module.exports = [
   {
     title: 'CSS Subgrid is here!',
     url: 'https://youtu.be/de2BY7g4YOg',
+    date: new Date(2024, 1, 3),
     cover: {
       folder: 'subgrid',
       filename: 'instagram-thumbnail.png',
@@ -22,5 +24,18 @@ module.exports = [
       width: 1080,
       height: 1080
     }
+  },
+  {
+    title: 'Baseline',
+    url: '/resources/baseline/',
+    date: new Date(2024, 5, 2),
+    cover: {
+      folder: 'links',
+      filename: 'baseline.png',
+      sizes: [300, 600, 900, 1080],
+      formats: ['png', 'webp', 'avif'],
+      width: 1080,
+      height: 1080
+    }
   }
-]
+].sort((a, b) => b.date - a.date).reverse()
